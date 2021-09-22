9c8a6c2028576556f36c375b54289814d2ae71aa
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-09-22 16:34:04 +0600

    shifted to cloud database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-import orderRouter from './routers/orderRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 
@@ -11,27 +11,36 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/giftCart', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
+mongoose
+  //   .connect(process.env.MONGODB_URL || "mongodb://localhost/giftCart", {
+  .connect(
+    "mongodb+srv://giftcartuser:giftcartmongo@cluster0.0coco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      dbName: "Gift_CartDB",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDB is connected......");
+  });
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-app.get('/', (req, res) =>{
-    res.send('Server is Ready');
+app.get("/", (req, res) => {
+  res.send("Server is Ready");
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({message: err.message});
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () =>{
-    console.log(`Serve at http://localhost:${port}`);
-}); 
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
