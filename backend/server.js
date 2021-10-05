31c2405d7647c72d15c2f1383e1241ee7ee549ee
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-10-05 12:37:16 +0600

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,10 @@ app.use("/api/orders", orderRouter);
 app.use("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-app.get("/", (req, res) => {
-  res.send("Server is Ready");
-});
+
+// app.get("/", (req, res) => {
+//   res.send("Server is Ready");
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
